6a10005f5dd5c1c064dedd9cd835a780b377f82e
Pradyumn Prakash Srivastava <102290399+Onaga08@users.noreply.github.com>
2025-10-27 12:37:06 +0530

    adding lib dependencies

--- formula/chat.rb
+++ formula/chat.rb
@@ -2,13 +2,27 @@ class Chat < Formula
   include Language::Python::Virtualenv
 
   desc "Real-time CLI chat client using WebSockets"
-  homepage "https://github.com/<yourusername>/chat_server"
-  url "https://github.com/Onaga08/CLI-Chat/archive/refs/tags/v1.0.0.tar.gz"
-  # https://github.com/Onaga08/CLI-Chat/archive/refs/tags/v1.0.0.tar.gz
-  sha256 "99c1f46574acddb2fabbb5b0cbf615e118ef8d3bc95d9989a4a7926e38d76e7d"
+  homepage "https://github.com/Onaga08/CLI-Chat"
+  url "https://github.com/Onaga08/CLI-Chat/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "<sha256>"
   license "MIT"
 
   depends_on "python@3.11"
+
+  resource "websockets" do
+    url "https://files.pythonhosted.org/packages/.../websockets-11.0.3.tar.gz"
+    sha256 "<sha256_of_wheel_or_tar.gz>"
+  end
+
+  resource "prompt_toolkit" do
+    url "https://files.pythonhosted.org/packages/.../prompt_toolkit-3.0.40.tar.gz"
+    sha256 "<sha256>"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/.../rich-13.3.2.tar.gz"
+    sha256 "<sha256>"
+  end
 
   def install
     virtualenv_install_with_resources
