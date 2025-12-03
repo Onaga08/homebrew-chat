51113bfc006b44c02b9c09e4724f2cdca66a4753
Pradyumn Prakash Srivastava <102290399+Onaga08@users.noreply.github.com>
2025-12-03 22:45:14 +0530

    Add Python dependencies for chat formula
    Added multiple Python dependencies for the chat formula.

--- formula/chat.rb
+++ formula/chat.rb
@@ -9,11 +9,54 @@ class Chat < Formula
 
   depends_on "python@3.11"
 
+  # ---------- Python deps (sdists you downloaded) ----------
+
+  resource "websockets" do
+    url "https://files.pythonhosted.org/packages/.../websockets-12.0.tar.gz"
+    sha256 "81df9cbcbb6c260de1e007e58c011bfebe2dafc8435107b0537f393dd38c8b1b"
+  end
+
+  resource "prompt_toolkit" do
+    url "https://files.pythonhosted.org/packages/.../prompt_toolkit-3.0.52.tar.gz"
+    sha256 "28cde192929c8e7321de85de1ddbe736f1375148b02f2e17edd840042b1be855"
+  end
+
+  resource "wcwidth" do
+    url "https://files.pythonhosted.org/packages/.../wcwidth-0.2.14.tar.gz"
+    sha256 "4d478375d31bc5395a3c55c40ccdf3354688364cd61c4f6adacaa9215d0b3605"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/.../rich-14.2.0.tar.gz"
+    sha256 "73ff50c7c0c1c77c8243079283f4edb376f0f6442433aecb8ce7e6d0b92d1fe4"
+  end
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/.../markdown_it_py-4.0.0.tar.gz"
+    sha256 "cb0a2b4aa34f932c007117b194e945bd74e0ec24133ceb5bac59009cda1cb9f3"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/.../mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "python-dotenv" do
+    url "https://files.pythonhosted.org/packages/.../python_dotenv-1.2.1.tar.gz"
+    sha256 "42667e897e16ab0d66954af0e60a9caa94f0fd4ecf3aaf6d2d260eec1aa36ad6"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/.../pygments-2.19.2.tar.gz"
+    sha256 "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
+  end
+
   def install
     virtualenv_install_with_resources
   end
 
   test do
+    # basic sanity check
     output = shell_output("#{bin}/chat --help")
     assert_match "usage", output
   end
