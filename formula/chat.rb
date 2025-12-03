9b42ac30bea86a3325c2cee040c864f490bef2cb
Pradyumn Prakash Srivastava <102290399+Onaga08@users.noreply.github.com>
2025-12-03 22:23:33 +0530

    Update chat.rb for v1.0.4

--- formula/chat.rb
+++ formula/chat.rb
@@ -1,17 +1,20 @@
 class Chat < Formula
   include Language::Python::Virtualenv
 
-  desc "Real-time CLI chat client using WebSockets"
-  homepage "https://github.com/<yourusername>/chat_server"
-  url "https://github.com/Onaga08/CLI-Chat/archive/refs/tags/v1.0.0.tar.gz"
-  # https://github.com/Onaga08/CLI-Chat/archive/refs/tags/v1.0.0.tar.gz
-  sha256 "99c1f46574acddb2fabbb5b0cbf615e118ef8d3bc95d9989a4a7926e38d76e7d"
+  desc "CLI and WebSocket-based chat application using FastAPI and prompt_toolkit"
+  homepage "https://github.com/Onaga08/CLI-Chat"
+  url "https://github.com/Onaga08/CLI-Chat/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "425af61e229dea8241c6edfae0260d99ca26a1b6943c4d1b09d86a080feff224"
   license "MIT"
 
   depends_on "python@3.11"
 
   def install
     virtualenv_install_with_resources
-    bin.install "chat.py" => "chat"
+  end
+
+  test do
+    output = shell_output("#{bin}/chat --help")
+    assert_match "usage", output
   end
 end
